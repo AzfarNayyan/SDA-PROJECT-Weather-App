0e37df75c3ccc0fee9e16fce7c788456ac39a095
TalhaTofeeq789 <163171281+TalhaTofeeq789@users.noreply.github.com>
2024-03-19 00:45:41 +0500

    Update WeatherAppDataBaseSchema.sql

--- WeatherAppDataBaseSchema.sql
+++ WeatherAppDataBaseSchema.sql
@@ -1,13 +1,19 @@
 create database SDAProject
 use SDAProject
+
+-- Drop the existing tables if they exist
+
+-- Create the Locations table with an auto-increment primary key
 CREATE TABLE Locations (
-  id INT PRIMARY KEY Not null,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   city VARCHAR(255) DEFAULT NULL,
   latitude DECIMAL(10,6) NOT NULL,
   longitude DECIMAL(10,6) NOT NULL
-)
+);
+
+-- Create the CurrentWeatherData table with a foreign key reference to Locations
 CREATE TABLE CurrentWeatherData (
-  id INT PRIMARY KEY Not null,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   location_id INT NOT NULL,
   feels_like DECIMAL(5,2) NOT NULL,
   temperature DECIMAL(5,2) NOT NULL,
@@ -16,13 +22,13 @@ CREATE TABLE CurrentWeatherData (
   humidity INT NOT NULL,
   sunrise TIME NOT NULL,
   sunset TIME NOT NULL,
+  city VARCHAR(30) NOT NULL,
   FOREIGN KEY (location_id) REFERENCES Locations(id)
 );
-ALTER TABLE CurrentWeatherData
-ADD city VARCHAR(30) NOT NULL;
 
+-- Create the AirPollutionData table with a foreign key reference to Locations
 CREATE TABLE AirPollutionData (
-  id INT PRIMARY KEY not null,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   location_id INT NOT NULL,
   timestamp DATETIME NOT NULL,
   aqi INT NOT NULL,
@@ -33,13 +39,22 @@ CREATE TABLE AirPollutionData (
   nh3 DECIMAL(5,2) DEFAULT NULL,
   FOREIGN KEY (location_id) REFERENCES Locations(id)
 );
+
+-- Create the ForecastData table with a foreign key reference to Locations
 CREATE TABLE ForecastData (
-  id INT PRIMARY KEY not null,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   location_id INT NOT NULL,
   forecast_date DATE NOT NULL, 
   min_temp DECIMAL(5,2) NOT NULL,
   max_temp DECIMAL(5,2) NOT NULL,
-  humidity int NOT NULL,
-  description varchar(50) NOT NULL,
+  humidity INT NOT NULL,
+  description VARCHAR(50) NOT NULL,
   FOREIGN KEY (location_id) REFERENCES Locations(id)
 );
+ALTER TABLE AirPollutionData MODIFY COLUMN co DECIMAL(10,2);
+
+select * from Locations;
+select * from CurrentWeatherData;
+select * from AirPollutionData;
+select * from ForecastData
+
