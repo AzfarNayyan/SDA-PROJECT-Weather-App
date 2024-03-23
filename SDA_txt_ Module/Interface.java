9d218f9ad0b2b2c9a07af036cebf1fda73f58c27
TalhaTofeeq789 <163171281+TalhaTofeeq789@users.noreply.github.com>
2024-03-24 02:34:12 +0500

    Create Interface.java

---
+++ SDA_txt_ Module/Interface.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONObject;
+
+
+public class DBInterface
+{
+     interface DataAccessInterface
+    {
+    void storeCurrentWeatherDataFromJson(JSONObject jsonData);
+    List<JSONObject> retrieveCurrentWeatherData(double latitude, double longitude);
+
+    void storeAirPollutionDataFromJson(JSONObject jsonData);
+    List<JSONObject> retrieveAirPollutionData(double latitude, double longitude);
+
+    void storeForecastDataFromJson(JSONObject jsonData);
+    List<JSONObject> retrieveForecastData(double latitude, double longitude);
+  }
+   
+}
