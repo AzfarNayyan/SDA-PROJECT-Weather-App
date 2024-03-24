f38ff5fe295959d78a6cf89283e9f1571c4b7a04
Saim Imran <116704886+loci123@users.noreply.github.com>
2024-03-24 19:45:48 +0500

    Update DBInterface.java

--- sdaDbModule/SDA_DBCon_NetBeans/src/dbconnection/DBInterface.java
+++ sdaDbModule/SDA_DBCon_NetBeans/src/dbconnection/DBInterface.java
@@ -5,15 +5,15 @@ import org.json.JSONArray;
 
 public interface DBInterface {
     interface DataAccessInterface {
-        void storeCurrentWeatherDataFromJson(JSONObject jsonData, String city);
+        void storeCurrentWeatherDataFromJson(JSONObject jsonData,String city,double longitude,double latitude);
 
         JSONObject retrieveCurrentWeatherData(double latitude, double longitude);
 
-        void storeAirPollutionDataFromJson(JSONObject jsonData, String city);
+        void storeAirPollutionDataFromJson(JSONObject jsonData,String city,double longitude,double latitude);
 
         JSONObject retrieveAirPollutionData(double latitude, double longitude);
 
-        void storeForecastDataFromJson(JSONObject jsonData, String city);
+        void storeForecastDataFromJson(JSONArray jsonData,String city,double longitude,double latitude);
 
         JSONArray retrieveForecastData(double latitude, double longitude);
 
